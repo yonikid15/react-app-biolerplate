4b28fc094616126f4d2368699012dbd721751175
Yonathan <yoni.kid15@gmail.com>
2018-04-17 14:57:42 -0400

    small update

--- webpack.config.js
+++ webpack.config.js
@@ -34,11 +34,6 @@ module.exports = (env) => {
                             sourceMap: true
                         }
                     },{
-                        loader: 'postcss-loader', 
-                        options: {
-                            sourceMap: true
-                        }
-                    },{
                         loader: 'sass-loader', 
                         options: {
                             sourceMap: true
